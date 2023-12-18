4f58157a05c3493ae80b6c83cff3cc523f20e185
VishalPachpor <Vishalpatil080502@gmail.com>
2023-12-18 17:00:36 +0530

    Card component done

--- components/VersusCard/Versus.jsx
+++ components/VersusCard/Versus.jsx
@@ -5,11 +5,14 @@ import SportSlider from './SportSlider';
 import VersusCard from './VersusCard';
 
 const sportsData = [
-  { name: 'Cricket', teams: ['Team A', 'Team B', 'Team C'] },
-  { name: 'Football', teams: ['Team X', 'Team Y', 'Team Z'] },
-  { name: 'Basketball', teams: ['Team D', 'Team E', 'Team F'] },
-  // Add more sports and teams as needed
+  { name: 'Cricket', teams: ['Team A', 'Team B', 'Team C'], date: '2023-01-20' },
+  { name: 'Football', teams: ['Team X', 'Team Y', 'Team Z'], date: '2023-01-21' },
+  { name: 'Basketball', teams: ['Team D', 'Team E', 'Team F'], date: '2023-01-22' },
 ];
+
+const generateRandomProbability = () => {
+  return (Math.random() * (5 - 0.5) + 0.5).toFixed(2);
+};
 
 const generateVersusData = (sportsData) => {
   const versusData = [];
@@ -21,6 +24,10 @@ const generateVersusData = (sportsData) => {
         versusData.push({
           team1: { name: teams[i], sport: sport.name, image: 'https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg' },
           team2: { name: teams[j], sport: sport.name, image: 'https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg' },
+          prob1: "2x",
+          prob2: "3x",
+          prob3: "4x",
+          date: sport.date,
         });
       }
     }
@@ -61,14 +68,21 @@ const VersusSlider = () => {
         sports={['All', ...sportsData.map((sport) => sport.name)]}
         onSportChange={handleSportChange}
       />
-      
+
       {sortedData.map((item, index) => (
         <VersusCard
           key={index}
           image1={item.team1.image}
-          text1={`${item.team1.name} (${item.team1.sport})`}
+          text1={`${item.team1.name} `}
           image2={item.team2.image}
-          text2={`${item.team2.name} (${item.team2.sport})`}
+          text2={`${item.team2.name} `}
+          team1={item.team1.name}
+          team2={item.team2.name}
+          prob1={item.prob1}
+          prob2={item.prob2}
+          prob3={item.prob3}
+          sportName={item.team1.sport}
+          date={item.date}
         />
       ))}
     </div>
--- components/VersusCard/VersusCard.jsx
+++ components/VersusCard/VersusCard.jsx
@@ -1,17 +1,58 @@
 // components/VersusCard.js
-const VersusCard = ({ image1, image2, text1, text2 }) => {
+const VersusCard = ({
+  image1,
+  image2,
+  text1,
+  text2,
+  team1,
+  team2,
+  prob1,
+  prob2,
+  prob3,
+  sportName,
+  date,
+}) => {
   return (
-    <div className=" bg-base-100 shadow-xl p-4 mb-4 text-center max-w-md mx-auto rounded-xl">
+    <div className="bg-base-100 shadow-xl p-4 mb-4 text-center max-w-md mx-auto rounded-xl">
+      {/* Heading with Sport Name and Date */}
+      <div className="flex items-center justify-between mb-2 w-full p-2 rounded">
+        <h2 className="text-2xl font-bold">{sportName} </h2>
+        <p className="text-gray-500">{date}</p>
+      </div>
+
       <div className="flex items-center justify-center mb-4">
         <div className="flex flex-col items-center mr-4">
-          <img src={image1} alt="Team 1" className="w-24 h-auto rounded-xl mb-2" />
+          <img
+            src={image1}
+            alt="Team 1"
+            className="w-32 h-auto rounded-xl mb-2"
+          />
           <p className="text-lg font-bold">{text1}</p>
         </div>
         <span className="text-2xl font-bold mx-4">vs</span>
         <div className="flex flex-col items-center ml-4">
-          <img src={image2} alt="Team 2" className="w-24 h-auto rounded-xl mb-2" />
+          <img
+            src={image2}
+            alt="Team 2"
+            className="w-32 h-auto rounded-xl mb-2"
+          />
           <p className="text-lg font-bold">{text2}</p>
         </div>
+      </div>
+      {/* Three buttons for teams and draw */}
+      <div className="flex justify-between gap-3">
+        <button className="result-button bg-transparent border-solid text-start border-2 border-gray-200 flex-grow items-center justify-center p-2 rounded-xl">
+          {team1}
+          <span className="text-purple-400"> {prob1}</span>
+        </button>
+        <button className="result-button bg-transparent border-solid text-start border-2 border-gray-200 flex-grow items-center justify-center p-2 rounded-xl">
+          Draw
+          <span className="text-purple-400"> {prob2}</span>
+        </button>
+        <button className="result-button bg-transparent border-solid text-start border-2 border-gray-200 flex-grow items-center justify-center p-2 rounded-xl">
+          {team2}
+          <span className="text-purple-400 text-end"> {prob3}</span>
+        </button>
       </div>
     </div>
   );
